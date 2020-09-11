c5e6c8b5df762a695b0e9a115a079168ebd31acf
Guipengtt <xgp980303@gmail.com>
2020-09-10 19:52:09 -0500

    Test

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('dist'));
+const port = process.env.PORT || 5000;
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("Tum Tum are running on 3000 port");
 })
